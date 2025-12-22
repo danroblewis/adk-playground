ea6acb2210e5b77d2db745639cad8d033ebb6d2d
Daniel Lewis <daniel.robert.lewis@gmail.com>
2025-12-21 16:30:15 -0800

    offline

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import './styles/fonts.css'
 import './styles/globals.css'
+import './monaco-config'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
